6a637674745ba3e15ac1d54a3907f992a68ba31e
bcbeltran <benjamin.beltran37@gmail.com>
2022-04-26 12:27:44 -0700

    Added Blockchain constructor function and createNewBlock function and tested that it works in the test.js file

---
+++ dev/blockchain.js
@@ -0,0 +1,27 @@
+function Blockchain() {
+    this.chain = [];
+    this.newTransactions = [];
+    this.difficulty = 5;
+    //this.miningJobs = [];
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, prevBlockHash, blockHash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        transactions: this.newTransactions,
+        difficulty: this.difficulty,
+        previousBlockHash: prevBlockHash,
+        //minedBy: ,
+        //blockDataHash: ,
+        nonce: nonce,
+        timestamp: Date.now(),
+        blockHash: blockHash
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+};
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const burbcoin = new Blockchain();
+
+burbcoin.createNewBlock(345136, "9f20932909hg0f9f82g2h0h923", "284h284fj920fh028f09jf3");
+burbcoin.createNewBlock(25346, "9f2093290902h0h923", "284h284fj274gf920fh028f09jf3");
+burbcoin.createNewBlock(7534516, "9f2093290727f87d9hg02h0h923", "284h284fj920fh028f09jf3");
+
+console.log(burbcoin);

9a4b224dec915840be5e3f798cf079e9228a61a0
bcbeltran <benjamin.beltran37@gmail.com>
2022-04-26 13:08:26 -0700

    Tested creating a block, creating a transaction, then creating a new block

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,20 @@ const Blockchain = require('./blockchain');
 const burbcoin = new Blockchain();
 
 burbcoin.createNewBlock(345136, "9f20932909hg0f9f82g2h0h923", "284h284fj920fh028f09jf3");
-burbcoin.createNewBlock(25346, "9f2093290902h0h923", "284h284fj274gf920fh028f09jf3");
-burbcoin.createNewBlock(7534516, "9f2093290727f87d9hg02h0h923", "284h284fj920fh028f09jf3");
 
+burbcoin.createNewTransaction("394gh284h8203392i9d", "2ryfhuh297380f8fwe8982", 48578392, "This is data that is sent with the transaction");
+
+console.log("These are the pending transactions before creating a new block: ");
+console.log(burbcoin.pendingTransactions);
+
+
+console.log("Creating new block...");
+burbcoin.createNewBlock(3745463, "9f202947g928h93f0h923", "82hf928f284h284fj920fh028f09jf3");
+
+
+console.log("These are the pending transactions after creating a new block: ");
+console.log(burbcoin.pendingTransactions);
+
+
+console.log("This is the entire current blockchain: ");
 console.log(burbcoin);

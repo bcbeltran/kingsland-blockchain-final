bd292acca673e0a65c9f63de177395aa79ad4b5c
bcbeltran <benjamin.beltran37@gmail.com>
2022-04-26 18:57:05 -0700

    Added endpoints for looking at the whole blockchain, creating a transaction and mining a block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,27 @@
 const sha256 = require('sha256');
+const uuid = require('uuid');
+
+const nodeAddress = uuid.v1().split('-').join('');
 
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
     this.difficulty = 5;
-    //this.miningJobs = [];
+    this.createNewTransaction("00", nodeAddress, 1000000000, "genesis block");
+    this.createNewBlock(0, "0", "0", nodeAddress, "0");
 }
 
-Blockchain.prototype.createNewBlock = function(nonce, prevBlockHash, blockHash) {
+Blockchain.prototype.createNewBlock = function(nonce, prevBlockHash, blockHash, minedBy, blockDataHash) {
     const newBlock = {
         index: this.chain.length + 1,
         transactions: this.pendingTransactions,
         difficulty: this.difficulty,
-        previousBlockHash: prevBlockHash,
-        //minedBy: ,
-        //blockDataHash: ,
+        prevBlockHash: prevBlockHash,
+        minedBy: minedBy,
+        blockDataHash: blockDataHash,
         nonce: nonce,
-        timestamp: new Date().toISOString(),
+        dateCreated: new Date().toISOString(),
+        timestamp: Date.now(),
         blockHash: blockHash
     };
 
@@ -31,11 +36,15 @@ Blockchain.prototype.getLastBlock = function() {
 };
 
 Blockchain.prototype.createNewTransaction = function(from, to, value, data) {
+    let txFee = 0;
+    if(from !== "00") {
+        txFee = 10;
+    }
     const newTransaction = {
         from: from,
         to: to,
         value: value,
-        fee: 10,
+        fee: txFee,
         dateCreated: new Date().toISOString(),
         data: data,
         // senderPubKey: ,
@@ -45,11 +54,24 @@ Blockchain.prototype.createNewTransaction = function(from, to, value, data) {
 
     this.pendingTransactions.push(newTransaction);
 
-    return this.getLastBlock()['index'] + 1;
+    if(this.chain.length === 0){
+        return;
+    } else {
+
+        return this.getLastBlock()['index'] + 1;
+    }
+};
+
+Blockchain.prototype.hashBlockData = function(currentBlockData) {
+    const dataString = JSON.stringify(currentBlockData);
+    const hash = sha256(dataString);
+    return hash;
 };
 
 Blockchain.prototype.hashBlock = function(prevBlockHash, currentBlockData, nonce) {
+    
     const dataString = prevBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    
     const hash = sha256(dataString);
     return hash;
 };
@@ -60,7 +82,7 @@ Blockchain.prototype.proofOfWork = function(prevBlockHash, currentBlockData) {
     while(hash.substring(0, this.difficulty) !== '00000') {
         nonce++;
         hash = this.hashBlock(prevBlockHash, currentBlockData, nonce);
-    };
+    }
 
     return nonce;
 };
--- dev/test.js
+++ dev/test.js
@@ -2,30 +2,4 @@ const Blockchain = require('./blockchain');
 
 const burbcoin = new Blockchain();
 
-
-const prevBlockHash = "294835720239094f3h2v9b8f2fj290f230";
-const currentBlockData = [
-    {
-        value: 10,
-        from: "298t74hf849fh380",
-        to: "2984h274hf892",
-        data: "tx 1"
-    },
-    {
-        value: 10,
-        from: "298t74hf849fh380",
-        to: "2984h274hf892",
-        data: "tx2"
-    },
-    {
-        value: 10,
-        from: "298t74hf849fh380",
-        to: "2984h274hf892",
-        data: "tx3"
-    },
-];
-
-//const pow = burbcoin.proofOfWork(prevBlockHash, currentBlockData);
-
-console.log(burbcoin.hashBlock(prevBlockHash, currentBlockData, 754234));
-//console.log("The nonce is: ", pow);
+console.log(burbcoin);
